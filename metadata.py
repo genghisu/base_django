2b74b3cab756311a760691809e5b9e1d67499e91
Han Xu <genghisu@gmail.com>
2015-12-08 19:30:30 -0500

    Init

---
+++ metadata.py
@@ -0,0 +1,20 @@
+import os
+
+try:
+    from __revision__ import __revision__
+except:
+    __revision__ = 'develop'
+
+metadata = {
+    'name': "base",
+    'version': "1.0",
+    'release': __revision__,
+    'url': 'https://hanbox.org',
+    'author': 'Han Mdarien',
+    'author_email': 'han.mdarien@gmail.com',
+    'admin': 'han.mdarien@gmail.com',
+    'dependencies': (
+    ),
+    'description': '',
+    'license': 'Private',
+}
